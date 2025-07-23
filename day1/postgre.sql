8c17450d4206525feb56469f842b65873565836f
Javohir <javohirsadullayev2024@gmail.com>
2025-07-23 20:19:23 +0500

    day1

---
+++ day1/postgre.sql
@@ -0,0 +1,17 @@
+-- CREATE TABLE values
+-- (
+--   id SERIAL PRIMARY KEY,
+--   name VARCHAR(100) NOT NULL,
+--   email VARCHAR(100) UNIQUE NOT NULL
+-- );
+
+-- ALTER TABLE values
+-- ADD descripticreate table users (
+--     id serial primary key,
+--     name varchar(100),
+--     email varchar(100) unique
+-- );
+
+-- DROP TABLE IF EXISTS jadval1;
+
+
